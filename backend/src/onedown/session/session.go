26f2cfd4715f2f064139250869225039e2f27257
Ashwin Vaidyanathan <vaidashwin@gmail.com>
2019-10-18 20:49:29 -0700

    moar work?? (on sessions)

--- backend/src/onedown/session/session.go
+++ backend/src/onedown/session/session.go
@@ -1,17 +1,27 @@
 package session
 
-import "github.com/TenjouUtena/onedown/backend/src/onedown/puzzle"
+import (
+	"github.com/TenjouUtena/onedown/backend/src/onedown/puzzle"
+	"github.com/google/uuid"
+	"github.com/wangjia184/sortedset"
+	"time"
+)
 
 type session struct {
 	puzz        *puzzle.Puzzle
 	channel     chan SessionMessage
 	state       puzzleState
-	solvers     []*solver
+	solvers     []*uuid.UUID
 	initialized bool
 }
 
 func doSession(sesh *session) {
-	// TODO: impl
+	for msg := range sesh.channel {
+		switch typedMsg := msg.(type) {
+		case WriteSquare:
+			sesh.state.putAnswer(typedMsg.player, typedMsg.row, typedMsg.col, typedMsg.answer)
+		}
+	}
 }
 
 func createSession(puzz *puzzle.Puzzle) *session {
@@ -27,10 +37,10 @@ func createSession(puzz *puzzle.Puzzle) *session {
 }
 
 type puzzleState struct {
+	filledSquares [][]*sortedset.SortedSet
 }
 
-type solver struct {
-}
-
-type filledSquare struct {
+func (state *puzzleState) putAnswer(solver uuid.UUID, row int, col int, answer string) {
+	now := sortedset.SCORE(time.Now().UnixNano())
+	state.filledSquares[row][col].AddOrUpdate(solver.String(), now, answer)
 }
--- backend/src/onedown/session/sessionmessage.go
+++ backend/src/onedown/session/sessionmessage.go
@@ -1,5 +1,13 @@
 package session
 
+import "github.com/google/uuid"
+
 type SessionMessage interface {}
 
-
+type WriteSquare struct {
+	SessionMessage
+	player uuid.UUID
+	row int
+	col int
+	answer string
+}
