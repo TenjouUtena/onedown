9ac5dd7edae132124974c9bf1a8e6b8866a7528c
CurtisSheu <sheu.curtis@gmail.com>
2019-10-21 23:44:22 -0400

    google oauth login endpoint finished
    Currently returns an html page with login button and then outputs to log that user's e-mail

--- backend/src/onedown/credentials/handlers.go
+++ backend/src/onedown/credentials/handlers.go
@@ -3,9 +3,21 @@ package credentials
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/oauth2"
 )
+
+//ConfigureHandlers Setup credentials api endpoints
+func ConfigureHandlers(router *gin.Engine) {
+	router.GET("/login", loginHandler)
+	router.GET("/oauth2callback", authHandler)
+}
 
 func getLoginURL(state string) string {
 	return conf.AuthCodeURL(state)
@@ -17,8 +29,38 @@ func randToken() string {
 	return base64.StdEncoding.EncodeToString(b)
 }
 
-func LoginHandler(c *gin.Context) {
-	state := randToken()
+func authHandler(c *gin.Context) {
+	session := sessions.Default(c)
+	retrievedState := session.Get("state")
+	if retrievedState != c.Query("state") {
+		c.AbortWithError(http.StatusUnauthorized, fmt.Errorf("Invalid session state: %s", retrievedState))
+		return
+	}
 
+	tok, err := conf.Exchange(oauth2.NoContext, c.Query("code"))
+	if err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	client := conf.Client(oauth2.NoContext, tok)
+
+	email, err := client.Get("https://www.googleapis.com/oauth2/v3/userinfo")
+	if err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+	defer email.Body.Close()
+
+	data, _ := ioutil.ReadAll(email.Body)
+	log.Println("Email body: ", string(data))
+	c.Status(http.StatusOK)
+}
+
+func loginHandler(c *gin.Context) {
+	state := randToken()
+	session := sessions.Default(c)
+	session.Set("state", state)
+	session.Save()
 	c.Writer.Write([]byte("<html><title>Golang Google</title> <body> <a href='" + getLoginURL(state) + "'><button>Login with Google!</button> </a> </body></html>"))
 }
